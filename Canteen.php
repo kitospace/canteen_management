ece587ad93602d56540daaa430e658c679ace27b
Puneet Kumar Rajan <32601094+kitospace@users.noreply.github.com>
2019-10-16 15:17:40 +0530

    Add files via upload

---
+++ Canteen.php
@@ -0,0 +1,26 @@
+<?php
+include('Canteen_Connection.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Canteen Data</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="js/new_bootstrap.js"></script>
+  <script src="js/bootstrap.js"></script>
+</head>
+<body>
+<?php
+include('Nav.php');
+?>
+  
+<div class="container">
+  <h1 align="Center">DASHBOARD</h1>   
+</div>
+
+</body>
+</html>
---
+++ header_menu.php
@@ -0,0 +1,21 @@
+<!-- Grey with black text -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav mr-auto">
+    <li class="nav-item active">
+      <a class="nav-link" href="register.php">New User</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="data.php">User List</a>
+    </li>
+	</ul>
+	<ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" href="">log In</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="" align ="Right">Sign Out</a>
+    </li>
+  </ul>
+</nav>
+
+
